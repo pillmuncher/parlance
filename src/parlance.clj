50cd19b309f089dae0bf6119e6d61bfb756b2444
Mick Krippendorf <m.krippendorf@freenet.de>
2018-10-22 12:54:58 +0200

    simplified some number parsers

--- src/parlance.clj
+++ src/parlance.clj
@@ -131,8 +131,8 @@
 
 (def positive-digit (->> "123456789" (map char) (reduce or-else)))
 (def digit (or-else positive-digit (char \0)))
-(def digits (action str (one-or-more digit)))
-(def positive-integer (action str (and-then positive-digit (opt digits))))
+(def digits (one-or-more digit))
+(def positive-integer (and-then positive-digit (opt digits)))
 (def opt-sign (opt (or-else (char \-) (char \+))))
 (def integer (action str (and-then opt-sign positive-integer)))
 (def decimal (action str (chain opt-sign integer (char \.) positive-integer)))
